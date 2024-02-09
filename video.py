3dd893d210b673a414d4b87e46c274f8387027a8
SammamMahdi <159187442+SammamMahdi@users.noreply.github.com>
2024-02-10 01:37:07 +0600

    Update video.py

--- video.py
+++ video.py
@@ -1,11 +1,16 @@
 import cv2
 from aruco_library import *
+
 cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
-    if(success):
-            Detected_ArUco_markers = detect_ArUco(img)	
-            angle = Calculate_orientation_in_degree(Detected_ArUco_markers)				## finding orientation of aruco with respective to the menitoned scale in problem statement
-            img = mark_ArUco(img,Detected_ArUco_markers,angle)
+    if success:
+        Detected_ArUco_markers = detect_ArUco(img)
+        angle = Calculate_orientation_in_degree(Detected_ArUco_markers)
+        theta1 = calc_theta1(
+            Detected_ArUco_markers
+        )  ## finding orientation of aruco with respective to the menitoned scale in problem statement
+        print(theta1)
+        img = mark_ArUco(img, Detected_ArUco_markers, angle, theta1)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
